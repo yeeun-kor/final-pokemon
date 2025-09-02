02b7de95a2be81e54aa782f0bea6d735f27b0bae
yeeun <jovm268@naver.com>
2025-09-02 14:30:38 +0900

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { FaMagnifyingGlass } from "react-icons/fa6";
 import { useDispatch } from "react-redux";
 import {
@@ -22,42 +22,34 @@ function App() {
     dispatch(fetchAllDataOfPokemonById(30));
   }, []);
 
+  //검색어 입력 쿼리스트링 활용
+  const navigate = useNavigate();
+  const location = useLocation();
+
   //검색어 상태 관리
   const [inputValue, setInputValue] = useState("");
 
-  //검색어 입력 쿼리스트링 활용
-  const navigate = useNavigate();
-
-  //페이지명 바뀔 씨 검색어 초기화
-  //useRef로 DOM 직접 건들이기 , state관리 노우
-  const location = useLocation();
-  const inputRef = useRef(null);
-
   useEffect(() => {
-    // if (inputRef.current) {
-    //   console.log("page reset");
-    //   inputRef.current.value = ""; // 직접 DOM 초기화
-    setInputValue("");
+    if (location.pathname !== "/search") {
+      setInputValue("");
+    }
   }, [location.pathname]);
 
   return (
     <>
       <h1 className="text-[40px] text-center">포켓몬 도감</h1>
       <nav className=" flex gap-3 justify-center">
-        <Link to={"/"} onClick={() => setInputValue("")}>
-          메인페이지
-        </Link>
+        <Link to={"/"}>메인페이지</Link>
         <Link to={"/favorite"}>찜목록</Link>
         <div className="flex items-center">
           <input
-            value={inputValue}
             type="text"
             placeholder="포켓몬검색"
-            className="border-b border-[darkgrey] px-2 w-32 "
-            //검색어 입력하면 바로 url이동 하도록
+            className="border-b border-[darkgrey] px-2 w-32"
+            value={inputValue}
             onChange={(e) => {
-              setInputValue(e.target.value);
-              navigate(`/search?pokemon=${e.target.value}`);
+              setInputValue(e.target.value); // 먼저 상태 반영
+              navigate(`/search?pokemon=${e.target.value}`); // 그리고 URL 반영
             }}
           />
           <FaMagnifyingGlass />
